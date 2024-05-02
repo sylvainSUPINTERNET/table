c737fa3a1c787550b62703677001c43483624703
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2024-05-02 23:08:48 +0200

    [WIP]

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const path = require('node:path');
 const folder = "D:\\Dev\\workspace\\pro\\1PP2_Prod_PMH_4464_Controle_25608_1\\P22";
 
 
+// JAVA parse : 
+
+// TODO : todo for both => table P22 AND ZZK9
+
+// String[] blocs = input.split("\n\n");
+
+// for ( String bloc : blocs ) {
+//     System.out.println(bloc);
+//     System.out.println("_______ ");
+// }
+
 
 let result = "String tableContent = \"";
 
