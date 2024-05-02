308908c77a6dd46d7ee8b6eb53e96ce289a577e4
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2024-05-02 22:50:20 +0200

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+const folder = "D:\\Dev\\workspace\\pro\\1PP2_Prod_PMH_4464_Controle_25608_1\\P22";
+
+
+
+let result = "String tableContent = '";
+
+fs.readdirSync(folder).map( fileName => {
+    return path.join(folder, fileName);
+}).forEach( filePath => { 
+    // read file
+    const data = fs.readFileSync(filePath, 'utf8');
+    
+    data.split("\n").forEach( line => {
+        result+= line+"\n";
+    })
+}); 
+ 
+result+= "';";
+
+fs.writeFileSync("./table.txt", result);
+console.log(result);
